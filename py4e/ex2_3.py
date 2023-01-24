492f7db383324fe38cb39c9578fbc85b6397f62a
MaliAbhishek <offcial.abhishek.ai@gmail.com>
2023-01-24 19:32:11 +0530

    Explanation added

--- py4e/ex2_3.py
+++ py4e/ex2_3.py
@@ -1,5 +1,5 @@
 hrs = input("Enter Hours:")
-hh=float(hrs)
+hh=float(hrs) #Input function stores information as a string so it is needed to convert it into float or integer
 rate=input("Enter Rate: ")
 rr=float(rate)
 pp=hh*rr
