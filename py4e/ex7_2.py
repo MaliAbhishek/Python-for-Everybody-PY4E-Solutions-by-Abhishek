9ab859d65df7830401ca5254f75045bf799f4433
MaliAbhishek <offcial.abhishek.ai@gmail.com>
2023-04-08 19:42:07 +0530

    Better Code

--- py4e/ex7_2.py
+++ py4e/ex7_2.py
@@ -1,15 +1,17 @@
-# Use the file name mbox-short.txt as the file name
-fname = input("Enter file name: ")
-fh = open(fname)
+#prompts for a file name
+file=input("Enter a file name:")
+#safety valve 
+try:
+    fhand=open(file) #opens that file and reads through the file
+except:
+    print("File is not readable. Please check the name and other parameteres", file)
+    quit()
 count=0
-xc=0
-for line in fh:
-    if not line.startswith("X-DSPAM-Confidence:"):
-        continue
-    #counting nuber of lines
-    count=count+1
-    #adding confidence level
-    Xconf=float(line[20:])
-    xc=xc+Xconf
-xav=xc/count
-print("Average spam confidence:", xav)
+total=0
+for i in fhand:
+    if i.startswith("X-DSPAM-Confidence:"): #ooking for lines of the form: X-DSPAM-Confidence:    0.8475
+        count+=1 #Count these lines and 
+        x=i.split(":")#extract the floating point values from each of the lines 
+        x=float(x[1])
+        total+=x
+print("Average spam confidence:", total/count) #and compute the average of those values and produce an output
